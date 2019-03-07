aeef1380e87dbaf3f9eb50211f92ecf367af470c
MininMobile <notminin@pm.me>
2019-03-07 22:54:52 +0000

    better logging

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-let http = require("http");
+const http = require("http");
 
 let server = http.createServer((request, response) => {
-	response.writeHead(200, { "Content-Type": "text/plain" });
+	console.log(`${time()} | request from ${request.socket.address().address}`);
+
+	let path = request.url.split("/").filter(str => str.length != 0);
+
+	response.writeHead(200, { "Content-Type": "text/html" });
 	response.end("Hello, World!");
 });
 
+server.on("listening", () => {
+	console.log("cyclic launched!");
+});
+
 server.listen(8080);
+
+function time() {
+	let d = new Date();
+	return `${d.getHours() + 1}:${d.getMinutes() + 1}:${d.getSeconds() + 1}`
+}
