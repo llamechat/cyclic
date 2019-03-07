058320ea084de8c318ff7024e1a3bbf2f81458b2
MininMobile <notminin@pm.me>
2019-03-07 22:24:28 +0000

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+let http = require("http");
+
+let server = http.createServer((request, response) => {
+	response.writeHead(200, { "Content-Type": "text/plain" });
+	response.end("Hello, World!");
+});
+
+server.listen(8080);
