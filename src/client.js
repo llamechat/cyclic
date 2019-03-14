5c5090a02d92500d468568e1802b52484107d804
MininMobile <notminin@pm.me>
2019-03-14 23:09:38 +0000

    add message reading

--- src/client.js
+++ src/client.js
@@ -12,9 +12,23 @@ sendButton.addEventListener("click", () => {
 		message: messageField.value,
 	})
 	.then((data) => {
-		display.innerText = JSON.stringify(JSON.parse(data), undefined, "\t");
+		console.log(JSON.parse(data));
 	}).catch(console.error);
 });
+
+(function getMessages() {
+	let c = () => setTimeout(getMessages, 2000);
+
+	get(`/api/channels/${channelField.value}/read/`).then((data) => {
+		display.innerText = JSON.stringify(JSON.parse(data), undefined, "\t");
+
+		c();
+	}).catch((e) => {
+		console.error(e);
+
+		c();
+	});
+})();
 
 function get(url){
 	return new Promise((resolve, reject) => {
